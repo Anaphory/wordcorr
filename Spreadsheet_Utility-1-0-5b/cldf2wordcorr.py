650b8b040f5df6323a38de3b79ae770920253160
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2017-04-03 13:50:46 +0200

    improve metadata reading

--- Spreadsheet_Utility-1-0-5b/cldf2wordcorr.py
+++ Spreadsheet_Utility-1-0-5b/cldf2wordcorr.py
@@ -25,9 +25,8 @@ def read_language_metadata(tsvfile):
     write_xml.
 
     """
-
     varieties = []
-    for variety in csv.DictReader(tsvfile, sep="\t"):
+    for variety in csv.DictReader(tsvfile, dialect="excel-tab"):
         varieties.append({
             "ethnologue": variety["ISO_code"],
             "name": variety["Language name (-dialect)"],
@@ -36,8 +35,8 @@ def read_language_metadata(tsvfile):
             "genclass": variety["Family"],
             "quality": '',
             "altname": '',
-            "locale": '{:f}, {:f}'.format(variety["Lat"],
-                                          variety["Lon"]),
+            "locale": '{:}, {:}'.format(variety["Lat"],
+                                        variety["Lon"]),
             "collection_country": variety["Region"],
             "collection_location": '',
             "collector": '',
@@ -48,25 +47,78 @@ def read_language_metadata(tsvfile):
     return varieties
 
 
+def read_concept_metadata(tsvfile, glosses=["English", "Indonesian"]):
+    """Read concept metadata from a tsv file.
+
+    Read a TSV file containing concept metadata (in particular
+    glosses) in specific columns, and convert it into the
+    three-list format expected by write_xml.
+
+    """
+    entries = []
+    gloss1 = []
+    gloss2 = []
+    for concept in csv.DictReader(tsvfile, dialect="excel-tab"):
+        entries.append(concept["Concept ID"])
+        gloss1.append(concept[glosses[0]])
+        gloss2.append(concept[glosses[1]])
+    return entries, gloss1, gloss2
+
+
 def read_collection_metadata(jsonfile):
     metadata = json.load(jsonfile)
-    [
-        "creatorrole",
-        "contributor",
-        "glosslg",
-        "lgcode1",
-        "glosslg2",
-        "lgcode2",
-        "keywords",
-        "description",
-        "remarks",
-        "published",
-        "coverage",
-        "stable",
-        "rights",
-        "copyright"]
+    for key in [
+            "creatorrole",
+            "contributor",
+            "glosslg",
+            "lgcode1",
+            "glosslg2",
+            "lgcode2",
+            "keywords",
+            "description",
+            "remarks",
+            "published",
+            "coverage",
+            "stable",
+            "rights",
+            "copyright"]:
+        metadata.setdefault(key, "")
     return metadata
 
+
+def read_data(all_data_tsv, languages, concepts):
+    """Read all counterpart data into the desired format.
+
+    Read an edictor tsv. The output is a list (per concept) of lists
+    (per language) of lists (per synonym) of strings.
+
+    """
+    languages_rl = {language["abbr"]: i
+                    for i, language in enumerate(languages)}
+    concepts_rl = {concept: i
+                   for i, concept in enumerate(concepts)}
+    all_data = [[[] for l in languages]
+                for c in concepts]
+
+    for row in csv.DictReader(all_data_tsv, dialect='excel-tab'):
+        try:
+            c = concepts_rl[row["CONCEPT_ID"]]
+        except KeyError:
+            # Try cutting of a floating point, otherwise assume we got
+            # a sublist of concepts and continue quietly.
+            if row["CONCEPT_ID"].endswith(".0"):
+                try:
+                    c = concepts_rl[row["CONCEPT_ID"][:-2]]
+                except KeyError:
+                    continue
+            else:
+                continue
+        try:
+            l = languages_rl[row["DOCULECT_ID"]]
+        except KeyError:
+            pass
+        all_data[c][l].append(row["ALIGNMENT"])
+    return all_data
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -83,6 +135,10 @@ if __name__ == '__main__':
         "languages", type=argparse.FileType("r"),
         default=sys.stdin,
         help="""The tsv file containing the languages metadata""")
+    parser.add_argument(
+        "concepts", type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="""The tsv file containing the concepts metadata""")
     parser.add_argument(
         "output", type=argparse.FileType("w"), nargs="?",
         default=sys.stdout,
@@ -102,9 +158,11 @@ if __name__ == '__main__':
 
     collection_data = read_collection_metadata(args.collection)
 
-    varietyproperties = read_language_metadata(args.languages)
+    languages = read_language_metadata(args.languages)
 
-    entries, gloss1, gloss2, data = ...
+    concepts, gloss1, gloss2 = read_concept_metadata(args.concepts)
 
-    write_xml(args.ouput, user_data, collection_data, varietyproperties,
-              entries, gloss1, gloss2, data)
+    data = read_data(args.data, languages, concepts)
+
+    write_xml(args.output, user_data, collection_data, languages,
+              concepts, gloss1, gloss2, data)
